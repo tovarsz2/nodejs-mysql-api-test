957f68e381ff781b2fe1a60554035771bef706cc
tovarsz2 <tovarszx@gmail.com>
2025-07-17 20:33:24 -0600

    add try catch

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,12 +3,20 @@ import { connectToDatabase } from '../db/db_connection.js'
 
 export const getPong = async (req, res) => {
 
-    connectToDatabase(`SELECT 'Pong' AS value`)
-        .then(result => {
-            res.json(result.recordset[0])
-        })
-        .catch(err => {
-            console.error('SQL error:', err)
-            res.status(500).send('Internal Server Error')
-        })
+    try {
+
+        connectToDatabase(`SELECT 'Pong' AS value`)
+            .then(result => {
+                res.json(result.recordset[0])
+            })
+            .catch(err => {
+                console.error('SQL error:', err)
+                res.status(500).send('Internal Server Error')
+            })
+
+    } catch (error) {
+        console.error('Error connecting to the database:', error)
+        res.status(500).send('Internal Server Error')
+        return
+    }
 }
